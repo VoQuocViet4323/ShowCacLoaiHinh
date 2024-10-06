a67cffe4f357006dffa1ae6f9dcdc4fd5cfc4b32
VoQuocViet4323 <140177566+VoQuocViet4323@users.noreply.github.com>
2024-10-06 22:57:53 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine($"Hinh chu nhat:");
+        int heightHCN = 3;
+        int widthHCN = 7;
+        for(int i = 0; i < heightHCN; i++){
+            for(int j = 0; j < widthHCN; j++){
+                Console.Write("* ");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+
+        Console.WriteLine($"Hinh tam giac vuong(bottom-left):");
+        int heightTGVa = 6;
+        for(int i = 0; i < heightTGVa; i++){
+            for(int j = 0; j < i; j++){
+                Console.Write("* ");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+
+        Console.WriteLine($"Hinh tam giac vuong(top-left):");
+        int heightTGVb = 5;
+        for(int i = heightTGVb; i >= 1; i--){
+            for(int j = 0; j < i; j++){
+                Console.Write("* ");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+}
